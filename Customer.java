cc8b0046fd698ec3ef91bd4a7dffbee6d05821c9
Pedro Cruz <pedrogabrielcruz00@gmail.com>
2022-08-07 10:16:20 -0300

    Commit5

--- Customer.java
+++ Customer.java
@@ -26,9 +26,6 @@ public class Customer {
 
         Rental each = (Rental) rentals.nextElement();
 
-        //determine amounts for each line
-        double thisAmount = each.getCharge();
-
         // add frequent renter points
         frequentRenterPoints ++;
         // add bonus for a two day new release rental
@@ -37,10 +34,11 @@ public class Customer {
 
         //show figures for this rental
         result += "\t" + each.getMovie().getTitle()+ "\t" +
-            String.valueOf(thisAmount) + "\n";
-        totalAmount += thisAmount;
+            (each.getCharge()) + "\n";
+        totalAmount += each.getCharge();
 
      }
+     
      //add footer lines
      result +=  "Amount owed is " + String.valueOf(totalAmount) + "\n";
      result += "You earned " + String.valueOf(frequentRenterPoints) +
