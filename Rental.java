12763897c525e31a8ed82e4a1081a63fa0c323c8
Pedro Cruz <pedrogabrielcruz00@gmail.com>
2022-08-07 11:22:46 -0300

    Commit10

--- Rental.java
+++ Rental.java
@@ -18,13 +18,9 @@ public class Rental {
 
     public double getCharge(){
         return _movie.getCharge(_daysRented);
-     }
+    }
 
-    public int getFrequentRenterPoints() {
-        int frequentRenterPoints = 1;
-        // add bonus for a two day new release rental
-        if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
-            getDaysRented() > 1) frequentRenterPoints ++;
-        return frequentRenterPoints;
+    public int getFrequentRenterPoints(){
+        return _movie.getFrequentRenterPoints(_daysRented);
     }
  }
